0af6dc21b3e785fbe076523dda7653026e1b68d1
dvavs <vavradylan@gmail.com>
2020-07-31 20:08:15 -0400

    Initial react skeleton commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Online home of Kalo the hero!
+          <br/>
+          <br/>
+          We're working on fixing the place up. Please check back soon!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
